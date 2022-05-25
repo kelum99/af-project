f0683633385382b469b045221d7273c6823a8c52
Bodhitha Weerasekara <88415912+IT20204334@users.noreply.github.com>
2022-05-26 01:02:10 +0530

    clr

--- af-project-fe/src/pages/Students/topicRegister.jsx
+++ af-project-fe/src/pages/Students/topicRegister.jsx
@@ -2,94 +2,106 @@ import React from 'react';
 import { Form, Input, Button } from 'antd';
 import './Students.css';
 
+ function TopicRegister() {
+//   const onReset = () => {
+//     form.resetFields();
+//   };
 
-   
-function TopicRegister() {
-    
-    const onReset = () => {
-        form.resetFields();
-      };
-    
+  // function TopicRegister() {
+  //   const [form] = Form.useForm();
+  //   const { request } = useRequest();
+  //   const [movies, setMovies] = useState([]);
+  //   const [selectedMovie, setSelectedMovie] = useState(undefined);
+  //   const [isModalVisible, setIsModalVisible] = useState(false);
+  //   const showModal = () => {
+  //     setIsModalVisible(true);
+  //   };
 
-    const onFinish = async (values) => {
-        try {
-          const res = await request.post("topic", values);
-          if (res.status === 201) {
-            message.success("Topic successfully added!");
-            console.log("move", res);
-            onReset();
-            getTopics();
-          } else {
-            message.error("failed!");
-          }
-        } catch (err) {
-          console.log("err", err);
-        }
-      };
+    // const handleCancel = () => {
+    //   setSelectedTopic(undefined);
+    //   setIsModalVisible(false);
+    // };
 
-return(
-    
+    // const onReset = () => {
+    //   form.resetFields();
+    // };
+
+  const onFinish = async (values) => {
+    try {
+      const res = await request.post('topic', values);
+      if (res.status === 201) {
+        message.success('Topic successfully added!');
+        console.log('move', res);
+        onReset();
+        getTopics();
+      } else {
+        message.error('failed!');
+      }
+    } catch (err) {
+      console.log('err', err);
+    }
+  };
+
+  // useEffect(() => {
+  //   getTopics();
+  //   handleCancel();
+  // }, []);
+
+  return (
     <div className="form-container1">
-    <Form layout="horizontal" labelCol={{ span: 10 }} wrapperCol={{ span: 8 }}
-     onFinish={onFinish}>
-      <h1 className='h1'>
-        <center>Topic Registertion</center>
-      </h1><br/>
+      <Form
+        layout="horizontal"
+        labelCol={{ span: 10 }}
+        wrapperCol={{ span: 8 }}
+        onFinish={onFinish}>
+        <h1 className="h1">
+          <center>Topic Registertion</center>
+        </h1>
+        <br />
 
-      
-
-      <Form.Item
+        <Form.Item
           label="Group ID"
           name="groupid"
           rules={[
             {
               required: true,
-              message: 'Please input your username!'
+              message: 'Please input your group id!'
             }
           ]}>
           <Input />
         </Form.Item>
 
-      <Form.Item
+        <Form.Item
           label="Research Topic"
           name="researchtopic"
           rules={[
             {
               required: true,
-              message: 'Please input your username!'
+              message: 'Please input your research topic!'
             }
           ]}>
           <Input.TextArea showCount maxLength={100} />
         </Form.Item>
 
-        
-        
-
-        
         <Form.Item
           label="Register Numbers"
           name="registernumbers"
           rules={[
             {
               required: true,
-              message: 'Please input your username!'
+              message: 'Please input your register numbers!'
             }
           ]}>
           <Input />
         </Form.Item>
 
         <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit" className='btn2'>
-              Submit
-            </Button>
-          </Form.Item>
-      
-  </Form>
-  </div>
-  
-);
-
-
-
+          <Button type="primary" htmlType="submit" className="btn2">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 export default TopicRegister;
